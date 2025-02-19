06407a65e9d1d0139a9e3a8b1c80ac486fd288aa
bdkamaci <burcudogakamaci@gmail.com>
2025-02-19 12:45:27 +0100

    chore: initialize Spring Boot application

--- src/main/java/com/bdkamaci/githubactivity/GithubActivityApplication.java
+++ src/main/java/com/bdkamaci/githubactivity/GithubActivityApplication.java
@@ -2,12 +2,14 @@ package com.bdkamaci.githubactivity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class GithubActivityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubActivityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActivityApplication.class, args);
+    }
 
 }
