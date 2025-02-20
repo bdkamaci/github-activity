defc1ab1db83d8b4f584d625f08bc4ada5c2d65c
bdkamaci <burcudogakamaci@gmail.com>
2025-02-20 22:53:22 +0100

    refactor(dto): Simplify ActivityDTO for Redis compatibility

--- src/main/java/com/bdkamaci/githubactivity/dto/ActivityDTO.java
+++ src/main/java/com/bdkamaci/githubactivity/dto/ActivityDTO.java
@@ -3,14 +3,15 @@ package com.bdkamaci.githubactivity.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ActivityDTO {
+public class ActivityDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String eventType;
-
     private String repoName;
-
     private String eventTime;
 }
